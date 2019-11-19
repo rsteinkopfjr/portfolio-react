6280503b34cc5433e3173b90b14c70a6733a8046
rsteinkopfjr <rob.steinkopf@gmail.com>
2019-11-19 15:38:10 -0500

    Add Take Flight

--- src/components/work.js
+++ src/components/work.js
@@ -2,54 +2,131 @@ import React, { Component } from "react";
 import Bakery from "../images/BakeryScreen.png";
 import Clicky from "../images/ClickyGameScreen.png";
 import Scraper from "../images/ScraperScreen.png";
+import Flight from "../images/TakeFlightScreen.png";
 import WebLogo from "../images/logos/WebWhite.png";
 import GitLogo from "../images/logos/GitHubWhite.png";
 
 class Work extends Component {
   render() {
     return (
-      <div className="container workSection">
-        <div className="row works">
-          <div className="card-deck">
-            <div className="card text-white">
-              <img src={Bakery} className="card-img-top" alt="..." />
-              <div className="card-body">
-                <h5 className="card-title">Taste and See Bakery</h5>
-                <p className="card-text">
-                  Website designed for a local bakery.
-                </p>
-              </div>
-              <div className="card-footer text-right">
-                  <img src={WebLogo} height="25" width="25" alt="Web Logo" className="cardFooterLogo" />
-                  <img src={GitLogo} height="25" width="25" alt="Github Logo" className="cardFooterLogo" />
-              </div>
+      <div className="container workSection works">
+        <div className="row card-deck">
+          <div className="card text-white">
+            <img src={Bakery} className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Taste and See Bakery</h5>
+              <p className="card-text">Website designed for a local bakery.</p>
             </div>
-            <div className="card text-white">
-              <img src={Clicky} className="card-img-top" alt="..." />
-              <div className="card-body">
-                <h5 className="card-title">Clicky Game</h5>
-                <p className="card-text">
-                  Bob's Burger themed game to test recollection and memory
-                </p>
-              </div>
-              <div className="card-footer text-right">
-                  <img src={WebLogo} height="25" width="25" alt="Web Logo" className="cardFooterLogo" />
-                  <img src={GitLogo} height="25" width="25" alt="Github Logo" className="cardFooterLogo" />
-              </div>
+            <div className="card-footer text-right">
+              <img
+                src={WebLogo}
+                height="25"
+                width="25"
+                alt="Web Logo"
+                className="cardFooterLogo"
+              />
+              <img
+                src={GitLogo}
+                height="25"
+                width="25"
+                alt="Github Logo"
+                className="cardFooterLogo"
+              />
             </div>
-            <div className="card text-white">
-              <img src={Scraper}className="card-img-top" alt="..." />
-              <div className="card-body">
-                <h5 className="card-title">News Scraper</h5>
-                <p className="card-text">
-                  All fantasy football news from BleacherReport.com in one location.
-                </p>
-              </div>
-              <div className="card-footer text-right">
-                  <img src={WebLogo} height="25" width="25" alt="Web Logo" className="cardFooterLogo" />
-                  <img src={GitLogo} height="25" width="25" alt="Github Logo" className="cardFooterLogo" />
-              </div>
+          </div>
+          <div className="card text-white">
+            <img src={Clicky} className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Clicky Game</h5>
+              <p className="card-text">
+                Bob's Burger themed game to test recollection and memory
+              </p>
             </div>
+            <div className="card-footer text-right">
+              <img
+                src={WebLogo}
+                height="25"
+                width="25"
+                alt="Web Logo"
+                className="cardFooterLogo"
+              />
+              <img
+                src={GitLogo}
+                height="25"
+                width="25"
+                alt="Github Logo"
+                className="cardFooterLogo"
+              />
+            </div>
+          </div>
+          <div className="card text-white">
+            <img src={Scraper} className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">News Scraper</h5>
+              <p className="card-text">
+                All fantasy football news from BleacherReport.com in one
+                location.
+              </p>
+            </div>
+            <div className="card-footer text-right">
+              <img
+                src={WebLogo}
+                height="25"
+                width="25"
+                alt="Web Logo"
+                className="cardFooterLogo"
+              />
+              <img
+                src={GitLogo}
+                height="25"
+                width="25"
+                alt="Github Logo"
+                className="cardFooterLogo"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="row card-deck">
+          <div className="card text-white">
+            <img src={Flight} className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Take Flight</h5>
+              <p className="card-text">Website designed for drone app.</p>
+            </div>
+            <div className="card-footer text-right">
+              <a
+                href="https://ancient-eyrie-50147.herokuapp.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src={WebLogo}
+                  height="25"
+                  width="25"
+                  alt="Web Logo"
+                  className="cardFooterLogo"
+                />
+              </a>
+              <a
+                href="https://github.com/rsteinkopfjr/TakeFlight"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src={GitLogo}
+                  height="25"
+                  width="25"
+                  alt="Github Logo"
+                  className="cardFooterLogo"
+                />
+              </a>
+            </div>
+          </div>
+          <div className="card cardEmpty text-white">
+  
+          </div>
+          <div className="card cardEmpty text-white">
+  
           </div>
         </div>
       </div>
